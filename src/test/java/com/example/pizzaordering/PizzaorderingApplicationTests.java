76f82bb51ed2d25e66c2eee06efc3572d97148df
Min-Uk.Lee <makerdark98@gmail.com>
2021-05-17 16:35:26 +0100

    Fix mysql test error

--- src/test/java/com/example/pizzaordering/PizzaorderingApplicationTests.java
+++ src/test/java/com/example/pizzaordering/PizzaorderingApplicationTests.java
@@ -2,8 +2,11 @@ package com.example.pizzaordering;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application.properties")
 class PizzaorderingApplicationTests {
 
     @Test
