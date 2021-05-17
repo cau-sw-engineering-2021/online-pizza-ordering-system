9977224c26eb5a58b14c268e7901bb0547114c56
Min-Uk.Lee <makerdark98@gmail.com>
2021-05-17 17:09:20 +0100

    Auto restart test

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Pizza Order System
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
