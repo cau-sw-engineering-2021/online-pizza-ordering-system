d8a1e4b87d0b59ea1de17d241bbb5fdbe01aaec4
alertjjm <sdr2936@gmail.com>
2021-05-23 12:26:13 +0900

    Fix Sales Mgmt

--- src/main/java/com/example/pizzaordering/controller/management/SaleController.java
+++ src/main/java/com/example/pizzaordering/controller/management/SaleController.java
@@ -5,16 +5,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import lombok.RequiredArgsConstructor;
-
-import java.sql.Date;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
-
-import com.example.pizzaordering.vo.Menu;
 import com.example.pizzaordering.vo.Order;
-import com.example.pizzaordering.vo.Item;
 
 @CrossOrigin("*")
 @RequiredArgsConstructor
@@ -29,10 +23,8 @@ public class SaleController {
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("start", start);
         result.put("end", end);
-        Date startD=Date.valueOf(start);
-        Date endD=Date.valueOf(end);
         result.put("msg", "success");
-        List<Order> sales = displaySalesOperator.displaySales(startD,endD);
+        List<Order> sales = displaySalesOperator.displaySales(start,end);
         result.put("sales", sales);
         retval = new ResponseEntity<Object>(result, HttpStatus.OK);
         return retval;
