0abaee428f3d8940ab02eb6b5165c4f35f798552
identity16 <dnjswns0930@gmail.com>
2021-05-23 12:15:45 +0900

    Add fonts for material-ui

--- src/main/webapp/src/menu-list/MenuList.js
+++ src/main/webapp/src/menu-list/MenuList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Typography } from "materia";
 
 function MenuList() {
   return (
     <main className="MenuList">
-      <h1>Menu List</h1>
+      <Typography variant="h1" component="h2">
+        h1. Heading
+      </Typography>
     </main>
   );
 }
