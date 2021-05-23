476d0ee9014ff91986a8ce2b0634fa66bb3180e3
Min-Uk.Lee <makerdark98@gmail.com>
2021-05-23 12:04:29 +0100

    Fix react routing

---
+++ src/main/java/com/example/pizzaordering/controller/common/ReactAppController.java
@@ -0,0 +1,14 @@
+package com.example.pizzaordering.controller.common;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactAppController {
+
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    public String getIndex(HttpServletRequest request) {
+        return "/index.html";
+    }
+}
