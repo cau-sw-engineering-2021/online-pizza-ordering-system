b5f4ef55ac0d856fa3ea6ea2c1bc47ba25cc932d
Min-Uk.Lee <makerdark98@gmail.com>
2021-05-23 05:35:13 +0100

    Add Subgroup C implementation(order status)

---
+++ src/main/java/com/example/pizzaordering/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.example.pizzaordering.repository;
+
+import com.example.pizzaordering.vo.Order;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+    public List<Order> findAllByOrderStatusIn(List<String> status);
+}
